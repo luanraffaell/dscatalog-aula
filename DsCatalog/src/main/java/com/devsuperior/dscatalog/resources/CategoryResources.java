76e72e6bf9cb2074549d7cbdccad790d44af0d24
Luan Rafael <luanraffaell@hotmail.com>
2022-09-16 17:23:36 -0300

    Find category by id, exception handler

--- DsCatalog/src/main/java/com/devsuperior/dscatalog/resources/CategoryResources.java
+++ DsCatalog/src/main/java/com/devsuperior/dscatalog/resources/CategoryResources.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +21,10 @@ public class CategoryResources {
 	@GetMapping
 	public ResponseEntity<List<CategoryDTO>> findAll(){
 		return ResponseEntity.ok().body(categoryService.findAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<CategoryDTO> findById(@PathVariable Long id){
+		return ResponseEntity.ok().body(categoryService.findById(id));
 	}
 }
--- DsCatalog/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ DsCatalog/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -10,6 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
+import com.devsuperior.dscatalog.services.exceptions.EntityNotFoundException;
 
 @Service //vai registrar com um componente spring, ou seja, o spring irá gerenciar a injeção de dependência dessa classe
 public class CategoryService {
@@ -23,5 +24,10 @@ public class CategoryService {
 		return list.stream().map(x -> new CategoryDTO(x))
 			.collect(Collectors.toList());
 		
+	}
+
+	public CategoryDTO findById(Long id) {
+		Category category = categoryRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Category not found"));
+		return new CategoryDTO(category);
 	}
 }
