b12b86651457ee826a81357a366c4817bddc4073
Luan Rafael <luanraffaell@hotmail.com>
2022-09-27 23:51:48 -0300

    custom constraintValidator

--- DsCatalog/src/main/java/com/devsuperior/dscatalog/resources/exceptions/ValidationError.java
+++ DsCatalog/src/main/java/com/devsuperior/dscatalog/resources/exceptions/ValidationError.java
@@ -7,12 +7,13 @@ public class ValidationError extends StandardError {
 	private static final long serialVersionUID = 1L;
 	
 	private List<FieldMessage> errors = new ArrayList<>();
+	
+	public void addError(String fieldName, String message) {
+		errors.add(new FieldMessage(fieldName, message));
+	}
 
 	public List<FieldMessage> getErrors() {
 		return errors;
 	}
 	
-	public void addError(String fieldName, String message) {
-		errors.add(new FieldMessage(fieldName, message));
-	}
 }
